38781f29f21a0d45e70c0f19f69c6ac5538f534d
Kyusik Shin <sks156324@gmail.com>
2016-08-15 16:38:37 +0900

    solve printpath

--- 4/printpath.py
+++ 4/printpath.py
@@ -40,6 +40,9 @@ def search(grid,init,goal,cost):
     # ----------------------------------------
     closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
     closed[init[0]][init[1]] = 1
+    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+    action = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+    ei = 0
 
     x = init[0]
     y = init[1]
@@ -61,6 +64,8 @@ def search(grid,init,goal,cost):
             x = next[1]
             y = next[2]
             g = next[0]
+            expand[x][y] = ei
+            ei += 1
             
             if x == goal[0] and y == goal[1]:
                 found = True
@@ -73,6 +78,20 @@ def search(grid,init,goal,cost):
                             g2 = g + cost
                             open.append([g2, x2, y2])
                             closed[x2][y2] = 1
+                            action[x2][y2] = i
+    
+    policy = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
+    x = goal[0]
+    y = goal[1]
+    policy[x][y] = '*'
+    while x != init[0] or y != init[1]:
+        x2 = x - delta[action[x][y]][0]
+        y2 = y - delta[action[x][y]][1]
+        policy[x2][y2] = delta_name[action[x][y]]
+        x = x2
+        y = y2
 
-    return expand # make sure you return the shortest path
+    return policy # make sure you return the shortest path
+
+
 
