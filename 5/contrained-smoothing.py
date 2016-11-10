dcb62b7c680d8f632f7d1fdc29240e905a50d244
Kyusik Shin <sks156324@gmail.com>
2016-11-10 21:23:45 +0900

    solve contrained smoothing

--- 5/contrained-smoothing.py
+++ 5/contrained-smoothing.py
@@ -17,12 +17,34 @@
 #
 
 ######################## ENTER CODE BELOW HERE #########################
+from copy import deepcopy
 
 def smooth(path, fix, weight_data = 0.0, weight_smooth = 0.1, tolerance = 0.00001):
     #
     # Enter code here. 
     # The weight for each of the two new equations should be 0.5 * weight_smooth
     #
+    newpath = deepcopy(path)
+    change = tolerance
+
+    while change >= tolerance:
+        change = 0.0 
+        for i in range(len(path)):
+            if not fix[i]:
+                for j in range(len(path[0])):
+                    aux = newpath[i][j]
+                    newpath[i][j] += weight_smooth * (newpath[(i-1)%len(path)][j] \
+                            + newpath[(i+1)%len(path)][j] \
+                            - 2.0 * newpath[i][j]) \
+                    + (weight_smooth / 2.0) * (2.0 * newpath[(i-1)%len(path)][j] \
+                    - newpath[(i-2)%len(path)][j] - newpath[i][j]) \
+                    + (weight_smooth / 2.0) * (2.0 * newpath[(i+1)%len(path)][j] \
+                    - newpath[(i+2)%len(path)][j] - newpath[i][j])
+
+                    change += abs(aux - newpath[i][j])
+                
+
+    
     return newpath
 
 # --------------
